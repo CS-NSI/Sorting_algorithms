ba7abbe9c3bb85023f55d1e27a03d19be81f5f72
CS-NSI <58935796+CS-NSI@users.noreply.github.com>
2019-12-16 11:36:39 +0100

    Add files via upload

---
+++ tri croissant.py
@@ -0,0 +1,14 @@
+def tri_croissant(t):
+    for j in range(1, len(t)):
+        T = t[j]
+        i = j - 1
+        while i>= 0 and t [i] > T:
+            t[i+1] = t[i]
+            i = i-1
+        t[i+1] = T
+    return t
+
+
+
+
+
