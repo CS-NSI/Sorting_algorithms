5bcb0e8a99f43c4c61a14a2ed42a50867694aa38
CS-NSI <58935796+CS-NSI@users.noreply.github.com>
2020-01-26 15:23:31 +0100

    Add files via upload

---
+++ belong1.py
@@ -0,0 +1,9 @@
+def belong(number, list_of_number):
+    reponse = False
+    i = 0
+    while not reponse and  i <= len(list_of_number):
+        if number = list_of_number[i]:
+            reponse True
+        else:
+            i = i+1
+    return reponse
